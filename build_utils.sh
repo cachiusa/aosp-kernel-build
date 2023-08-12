e0c4afdeef5c683154b80d868d3f00aa0f279382
cachiusa <70309181+cachiusa@users.noreply.github.com>
2023-08-12 11:06:31 +0000

    _setup_env: Move all funcs to build_utils

--- build_utils.sh
+++ build_utils.sh
@@ -1007,3 +1007,21 @@ if key in js:
     echo "WARNING: Can't determine $what for $2" >&2
   fi
 }
+# verifies that defconfig matches the DEFCONFIG
+function check_defconfig() {
+    (cd ${OUT_DIR} && \
+     make ${TOOL_ARGS} O=${OUT_DIR} savedefconfig)
+    [ "$ARCH" = "x86_64" -o "$ARCH" = "i386" ] && local ARCH=x86
+    RES=0
+    if [[ -f ${KERNEL_DIR}/arch/${ARCH}/configs/${DEFCONFIG} ]]; then
+      diff -u ${KERNEL_DIR}/arch/${ARCH}/configs/${DEFCONFIG} ${OUT_DIR}/defconfig >&2 ||
+        RES=$?
+    else
+      diff -u ${OUT_DIR}/arch/${ARCH}/configs/${DEFCONFIG} ${OUT_DIR}/defconfig >&2 ||
+        RES=$?
+    fi
+    if [ ${RES} -ne 0 ]; then
+        echo ERROR: savedefconfig does not match ${KERNEL_DIR}/arch/${ARCH}/configs/${DEFCONFIG} >&2
+    fi
+    return ${RES}
+}
