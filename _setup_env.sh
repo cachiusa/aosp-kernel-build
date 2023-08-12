055d984e764cb02054f2ad49620d505264137c97
cachiusa <70309181+cachiusa@users.noreply.github.com>
2023-08-12 11:06:31 +0000

    Move all funcs to build_utils

--- _setup_env.sh
+++ _setup_env.sh
@@ -265,23 +265,3 @@ else
   RAMDISK_DECOMPRESS="${DECOMPRESS_LZ4}"
   RAMDISK_EXT="lz4"
 fi
-
-# verifies that defconfig matches the DEFCONFIG
-function check_defconfig() {
-    (cd ${OUT_DIR} && \
-     make ${TOOL_ARGS} O=${OUT_DIR} savedefconfig)
-    [ "$ARCH" = "x86_64" -o "$ARCH" = "i386" ] && local ARCH=x86
-    RES=0
-    if [[ -f ${KERNEL_DIR}/arch/${ARCH}/configs/${DEFCONFIG} ]]; then
-      diff -u ${KERNEL_DIR}/arch/${ARCH}/configs/${DEFCONFIG} ${OUT_DIR}/defconfig >&2 ||
-        RES=$?
-    else
-      diff -u ${OUT_DIR}/arch/${ARCH}/configs/${DEFCONFIG} ${OUT_DIR}/defconfig >&2 ||
-        RES=$?
-    fi
-    if [ ${RES} -ne 0 ]; then
-        echo ERROR: savedefconfig does not match ${KERNEL_DIR}/arch/${ARCH}/configs/${DEFCONFIG} >&2
-    fi
-    return ${RES}
-}
-export -f check_defconfig
